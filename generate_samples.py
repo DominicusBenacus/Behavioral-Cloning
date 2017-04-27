96367127442749f1e5b097fdd304afede8b1e9af
DomenicusBenacus <dominik.marquardt@outlook.de>
2017-04-27 18:31:42 +0200

    change numpy methods

--- generate_samples.py
+++ generate_samples.py
@@ -44,7 +44,7 @@ def generate_samples(data, augment=True, batch_size = 128):
                 image = cv2.imread(batch_sample[camera].strip())
                 steering_angle = float(batch_sample[STEERING_ANGLE]) + left_right_steering_correction[camera]
                 print(" value of steering angle: {}:".format(steering_angle))
-	            x = np.append(x, [image], axis=0)
+                x = np.append(x, [image], axis=0)
                 y = np.append(y, [steering_angle])
                 #x = np.append(x,image)
                 #y = np.append(y,steering_angle)
