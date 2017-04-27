ef75fc5537ca7e8b9a3dc3de10eab7770d1aed70
DomenicusBenacus <dominik.marquardt@outlook.de>
2017-04-27 18:36:23 +0200

    change numpy methods

--- generate_samples.py
+++ generate_samples.py
@@ -31,10 +31,12 @@ def generate_samples(data, augment=True, batch_size = 128):
             batch_samples = samples[batch:(batch + batch_size)]
             # Output arrays
             
-            x = np.empty([0, 160, 320, 3], dtype=np.float32)
+            images[]
+            steering_angles[]
+            #x = np.empty([0, 160, 320, 3], dtype=np.float32)
             x_shape = x.shape
             print("image shape after creation of np.empty array = ", x_shape)
-            y = np.empty([0], dtype=np.float32)
+            #y = np.empty([0], dtype=np.float32)
             # Read in and preprocess a batch of images
             for batch_sample in batch_samples:
                 # Randomly select camera
@@ -44,20 +46,23 @@ def generate_samples(data, augment=True, batch_size = 128):
                 image = cv2.imread(batch_sample[camera].strip())
                 steering_angle = float(batch_sample[STEERING_ANGLE]) + left_right_steering_correction[camera]
                 print(" value of steering angle: {}:".format(steering_angle))
-                x = np.append(x, [image], axis=0)
-                y = np.append(y, [steering_angle])
-                #x = np.append(x,image)
-                #y = np.append(y,steering_angle)
+                #x = np.append(x, [image], axis=0)
+                #y = np.append(y, [steering_angle])
+                images = np.append(image)
+                steering_angles = np.append(steering_angle)
+            
+            x = np.array(image)
+            y = np.array(steering_angle)
             x_shape = x.shape
-            print("Image data shape ater np.append(x,[image],axis=0)=", x_shape)
+            print("Image data shape after vrate numpy array ", x_shape)
 
             # Randomly flip half of images in the batch
             flip_indices = random.sample(range(x.shape[0]), int(x.shape[0] / 2))
-            #x[flip_indices] = np.fliplr(x[flip_indices])
+            x[flip_indices] = np.fliplr(x[flip_indices])
             #x[flip_indices] = cv2.flip(x[flip_indices],1)
-            #y[flip_indices] = -y[flip_indices]
-            x[flip_indices] = x[flip_indices, :, ::-1, :]
             y[flip_indices] = -y[flip_indices]
+            #x[flip_indices] = x[flip_indices, :, ::-1, :]
+            #y[flip_indices] = -y[flip_indices]
             x_shape = x.shape
             print("Image data shape after flipping images vertical =", x_shape)
             yield (x, y)
