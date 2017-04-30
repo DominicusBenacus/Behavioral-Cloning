0928af574abca6b5b544b58465f3f8958b664412
DomenicusBenacus <dominik.marquardt@outlook.de>
2017-04-30 14:45:26 +0200

    add balancing with csv reader

--- balancing.py
+++ balancing.py
@@ -1,9 +1,5 @@
 import csv
-import cv2
-import numpy as np
 import os
-import pandas as pd
-
 print('start of balancing')
 #                                                cam,    cam,  cam 
 # read in the recoded data set froma csv fromat [center, left, right, steering, throttle, brake, speed]
@@ -11,21 +7,13 @@ with open("../data/driving_log.csv", "rb") as infile, open("../data/driving_log_
    reader = csv.reader(infile)
    next(reader, None)  # skip the headers
    writer = csv.writer(outfile)
-   for row in reader:       
-        measurement = float(row[3])
-       # process each row
-       writer.writerow(row)
-
-print(" shape of the first row of samples after imread: {}:".format(samples[0]))
-
-for line in range(len(samples)):
-    
+   for row in reader:
+        steering_angle = float(row[3])
+        if steering_angle <= abs(0.05) in row:
+            continue# process each row
+        writer.writerow(row)
 
 #balance data and save it to a neew csv fil
-
-
-print('end of balancing')    
-balanced.to_csv('data/driving_log_balanced.csv', index=False)
 print('balanced data set saved to data/driving_log_balanced.csv')    
 print('========================================================')
 print('balanceing process done')
