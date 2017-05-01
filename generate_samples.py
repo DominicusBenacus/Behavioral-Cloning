35a9ac8b9ddb8bad326e69f3fd8a94391876d80c
DomenicusBenacus <dominik.marquardt@outlook.de>
2017-05-01 17:12:49 +0200

    add split function ti get the correct source path to images according to th different OS

--- generate_samples.py
+++ generate_samples.py
@@ -39,8 +39,9 @@ def generate_samples(samples, augment=True, batch_size=128):
             for batch_sample in batch_samples:
                 for i in range(0, 3):
                     source_path = batch_sample[i]
-                    filename = source_path.split('\\')[-1]
-                    current_path = '../data/IMG/'+source_path.split('/')[-1]
+                    filename = os.path.split(source_path)
+                    #filename = source_path.split('\\')[-1]
+                    current_path = os.path.join('../data/IMG/',filename)
                     #current_path = '../data/IMG/' + filename
                     image = cv2.imread(current_path)
 
