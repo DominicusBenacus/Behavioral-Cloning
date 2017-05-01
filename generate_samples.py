70bdf6fc4f53025163b8bc3a4b4cab66b5227a66
DomenicusBenacus <dominik.marquardt@outlook.de>
2017-05-01 20:07:27 +0200

    add random selection of camera

--- generate_samples.py
+++ generate_samples.py
@@ -5,14 +5,13 @@ import cv2
 import os
 from random import shuffle
 import sklearn
-from numpy.random import uniform
-
- 
+from numpy.random import uniform, randint
 
 
 # set up some parameter for dealing with the different camera positions
 left_right_steering_correction = [0, .25, -.25]
 STEERING_ANGLE = 3
+cameras = [0, 1, 2]
 
 
 def generate_samples(samples, augment=True, batch_size=128):
@@ -40,35 +39,36 @@ def generate_samples(samples, augment=True, batch_size=128):
             steering_angles = []
             # Read in and preprocess a batch of images
             for batch_sample in batch_samples:
-                for i in range(0, 3):
-                    source_path = batch_sample[i]
-                    #source_path = source_path.split('/')[-1]
-                    filename = source_path.split('\\')[-1]
-                    #current_path = os.path.join('../data/IMG/',filename)
-                    current_path = '../data/IMG/' + filename
-                    image = cv2.imread(current_path)
+                # for i in range(0, 3):
+                camera = np.random.randint(len(cameras)) if augment else 1
 
-                    # randomize brightness
-                    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-                    image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-                    random_brightness = .1 + np.random.uniform()
-                    image[:, :, 2] = image[:, :, 2] * random_brightness
-                    image = cv2.cvtColor(image, cv2.COLOR_HSV2RGB)
+                source_path = batch_sample[camera]
+                #source_path = source_path.split('/')[-1]
+                filename = source_path.split('\\')[-1]
+                #current_path = os.path.join('../data/IMG/',filename)
+                current_path = '../data/IMG/' + filename
+                image = cv2.imread(current_path)
 
-                    images.append(image)
-                    # read steering angle depending an add offset depending on
-                    # the camera possition
-                    steering_angle = float(batch_sample[STEERING_ANGLE])
-                    #print(" Shape of steering angle ater read in: {}:".format(steering_angle))
-                    steering_angle = float(steering_angle) + float(left_right_steering_correction[i])
-                    #print(" Shape of steering angle after add or sub wor out: {}:".format(steering_angle))
-                    steering_angles.append(steering_angle)
-            
-            
-            print('this is the format of current path',current_path)
-            print('this is the format of filename',filename)
-            print('this is the format of source_path',source_path)
+                # randomize brightness
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+                random_brightness = .1 + np.random.uniform()
+                image[:, :, 2] = image[:, :, 2] * random_brightness
+                image = cv2.cvtColor(image, cv2.COLOR_HSV2RGB)
 
+                images.append(image)
+                # read steering angle depending an add offset depending on
+                # the camera possition
+                steering_angle = float(batch_sample[STEERING_ANGLE])
+                #print(" Shape of steering angle ater read in: {}:".format(steering_angle))
+                steering_angle = float(
+                    steering_angle) + float(left_right_steering_correction[camera])
+                #print(" Shape of steering angle after add or sub wor out: {}:".format(steering_angle))
+                steering_angles.append(steering_angle)
+
+            print('this is the format of current path', current_path)
+            print('this is the format of filename', filename)
+            print('this is the format of source_path', source_path)
 
             #print(" Shape of steering angle after append: {}:".format(steering_angles))
             # create a numpy array because keras expect a numpy array as input
@@ -83,7 +83,7 @@ def generate_samples(samples, augment=True, batch_size=128):
             x[flip_indices] = np.fliplr(x[flip_indices])
             # #x[flip_indices] = cv2.flip(x[flip_indices],1)
             y[flip_indices] = -y[flip_indices]
-            
+
             x_shape = x.shape
             print("Image data shape after flipping images vertical =", x_shape)
             yield sklearn.utils.shuffle(x, y)
--- model.py
+++ model.py
@@ -122,10 +122,8 @@ def save_model(name):
 numTimes = 1
 val_best = 999
 # preload e weights if u train the model after append more traing data makes training faster.
-model = load_model('model.h5')
-print(model.summary())
-
-#model = architecture()
+#model = load_model('model.h5')
+model = architecture()
 num_epochs= 10
 batch_size = 128
 
